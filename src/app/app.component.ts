19490fa48a77d6c900daae3ae9742a4adeea8d90
andrej.ljashok <andrej.ljashok@stfalcon.com>
2018-04-05 11:52:36 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Model } from './model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-todo',
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app';
+  model = new Model();
+  getName() {
+    return this.model.user;
+  }
+  getTodoItems() {
+    return this.model.items;
+  }
 }
